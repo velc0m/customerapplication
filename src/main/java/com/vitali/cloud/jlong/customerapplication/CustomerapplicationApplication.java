b0ad6ba75dbdf20481bd8d35d66260e1204c297a
Vitali Litvinenka <lvitali@setplex.com>
2018-12-02 10:54:59 +0300

    Initial commit

---
+++ src/main/java/com/vitali/cloud/jlong/customerapplication/CustomerapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.vitali.cloud.jlong.customerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerapplicationApplication.class, args);
+    }
+}
