a5792a9314203239c4bcfa9ea80a0058a6e7c80b
Vitali Litvinenka <lvitali@setplex.com>
2018-12-09 15:42:47 +0300

    Changed ctrl for get remote message.

--- src/main/java/com/vitali/cloud/jlong/customerapplication/CustomerRestController.java
+++ src/main/java/com/vitali/cloud/jlong/customerapplication/CustomerRestController.java
@@ -1,6 +1,7 @@
 package com.vitali.cloud.jlong.customerapplication;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,18 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class CustomerRestController {
 
+    @Value("${project.message}")
+    private String message;
+
     private final CustomerService customerService;
 
     @GetMapping("/customers")
     public Collection<Customer> getAll() {
         return customerService.findAll();
+    }
+
+    @GetMapping("/message")
+    public String getRemoteMessage() {
+        return message;
     }
 }
