f03fa00d6bdea405702145b43ad1344df3b7e679
Vitali Litvinenka <lvitali@setplex.com>
2018-12-09 16:23:27 +0300

    Added @RefreshScope.

--- src/main/java/com/vitali/cloud/jlong/customerapplication/CustomerRestController.java
+++ src/main/java/com/vitali/cloud/jlong/customerapplication/CustomerRestController.java
@@ -2,6 +2,7 @@ package com.vitali.cloud.jlong.customerapplication;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.Collection;
 
 @RestController
 @RequiredArgsConstructor
+@RefreshScope
 public class CustomerRestController {
 
     @Value("${project.message}")
